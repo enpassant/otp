171e9526c2e36741081d6d5d02e3567a94cff448
Enpassant <enpassant.prog@gmail.com>
2024-05-03 07:31:19 +0200

    Separate stdout and stderr

--- example/main.zig
+++ example/main.zig
@@ -1,13 +1,63 @@
 const std = @import("std");
+const fs = std.fs;
 const otp = @import("otp");
 const print = std.debug.print;
 
 pub fn main() !void {
-    const hotp = otp.Hotp.init();
-    const code = try hotp.generateCode("secretkey", 0);
-    print("code: {}\n", .{code});
+    const file = try fs.cwd().openFile("otp.rc", .{});
+    defer file.close();
 
-    const totp = otp.Totp.init(otp.Options{});
-    const totp_code = try totp.generateCode("secretkey", std.time.timestamp());
-    print("code: {}\n", .{totp_code});
+    var line: [200]u8 = undefined;
+    var key: ?[]const u8 = null;
+    var issuer: ?[]const u8 = null;
+    var key_buf: [20]u8 = undefined;
+    var buf: [8]u8 = undefined;
+    var options: otp.Options = .{};
+
+    while (try file.read(&line) > 0) {
+        var iter = std.mem.tokenize(u8, &line, " :=\r\n");
+        while (iter.next()) |token| {
+            if (std.mem.eql(u8, token, "key")) {
+                key = iter.next();
+            } else if (std.mem.eql(u8, token, "Sha1")) {
+                options.algorithm = .Sha1;
+            } else if (std.mem.eql(u8, token, "Sha256")) {
+                options.algorithm = .Sha256;
+            } else if (std.mem.eql(u8, token, "6")) {
+                options.digits = 6;
+            } else if (std.mem.eql(u8, token, "7")) {
+                options.digits = 7;
+            } else if (std.mem.eql(u8, token, "8")) {
+                options.digits = 8;
+            } else if (std.mem.eql(u8, token, "30")) {
+                options.time_step = 30;
+            } else if (std.mem.eql(u8, token, "60")) {
+                options.time_step = 60;
+            } else if (std.mem.eql(u8, token, "issuer")) {
+                issuer = iter.next();
+            } else if (std.mem.eql(u8, token, "options") or token.len == 0 or token[0] == 0) {} else {
+                print("Unknown token: {s}\n", .{token});
+            }
+
+            if (key != null and issuer != null) {
+                const totp = otp.Totp.init(&buf, options);
+
+                var seed = otp.decodeBase32(&key_buf, key.?);
+                var totp_code = try totp.generateCode(seed, std.time.timestamp());
+                var remaining_time = totp.remainingTime(std.time.timestamp());
+                print("{s}: {s}, {d}s\n", .{
+                    issuer.?,
+                    totp_code,
+                    remaining_time,
+                });
+                try std.io.getStdOut().writer().print("{s}: {s}\n", .{
+                    issuer.?,
+                    totp_code,
+                });
+                issuer = null;
+                key = null;
+                options = .{};
+            }
+        }
+    }
 }
--- src/otp.zig
+++ src/otp.zig
@@ -32,17 +32,25 @@ pub const Hotp = struct {
     const Self = @This();
 
     digits: u8 = 6,
+    buffer: []u8 = undefined,
 
     /// Init creates a new Hotp struct with the `digits` set by default to 6.
-    pub fn init() Self {
-        return .{};
+    pub fn init(buf: []u8) Self {
+        return .{ .buffer = buf };
     }
 
     /// generateCode creates a new code using the given secret.
     /// The counter needs to be synchronized between the client and server.
     /// It is up to the implementation to handle the synchronization, this library does not facilitate it.
     pub fn generateCode(self: Self, secret: []const u8, counter: u64) ![]u8 {
-        return buildCode(secret, counter, self.digits, Algorithm.Sha1);
+        const result = buildCode(
+            self.buffer,
+            secret,
+            counter,
+            self.digits,
+            Algorithm.Sha1,
+        );
+        return result;
     }
 };
 
@@ -53,23 +61,63 @@ pub const Totp = struct {
     const Self = @This();
 
     opts: Options,
+    buffer: []u8 = undefined,
 
     /// Init creates a new Totp struct and handles the generated codes according to it.
-    pub fn init(opts: Options) Self {
-        return .{ .opts = opts };
+    pub fn init(buf: []u8, opts: Options) Self {
+        return .{ .buffer = buf, .opts = opts };
     }
 
     /// generateCode creates a new code with a length of `digits`.
     /// `timestamp` can be generated using `std.milliTimestamp`.
     pub fn generateCode(self: Self, secret: []const u8, time: i64) ![]u8 {
-        // Convert to floats for higher precision
-        const counter = @intToFloat(f64, @bitCast(u64, time)) / @intToFloat(f64, self.opts.time_step);
-        return buildCode(secret, @floatToInt(u64, std.math.floor(counter)), self.opts.digits, self.opts.algorithm);
+        const counter: u64 = @intCast(@divTrunc(time, self.opts.time_step));
+        return buildCode(
+            self.buffer,
+            secret,
+            counter,
+            self.opts.digits,
+            self.opts.algorithm,
+        );
+    }
+
+    pub fn remainingTime(self: Self, time: i64) u8 {
+        return self.opts.time_step -
+            @as(u8, @intCast(@rem(time, self.opts.time_step)));
     }
 };
 
+fn charToBits(ch: u8) u5 {
+    if (ch >= 'A' and ch <= 'Z')
+        return @truncate(ch - 'A')
+    else
+        return @truncate(26 + ch - '2');
+}
+
+pub fn decodeBase32(out_buf: []u8, text: []const u8) []const u8 {
+    const rem = @rem(text.len, 8);
+    const pad_len = @divTrunc(rem * 5, 8);
+    const len = @divTrunc(text.len, 8) * 5 + pad_len;
+    var pos: usize = 0;
+    var num: u16 = 0;
+    var bit_pos: u4 = 11;
+    for (text) |ch| {
+        const bits: u5 = charToBits(ch);
+        num |= @shlExact(@as(u16, bits), bit_pos);
+        if (bit_pos > 8)
+            bit_pos -= 5
+        else {
+            out_buf[pos] = @truncate((num & 0xff00) >> 8);
+            num = (num & 0xff) << 8;
+            bit_pos += 8 - 5;
+            pos += 1;
+        }
+    }
+    return out_buf[0..len];
+}
+
 /// generateCode creates the actual code given the provided parameters from the `Hotp` & `Totp` structs.
-fn buildCode(secret: []const u8, counter: u64, digits: u8, algorithm: Algorithm) ![]u8 {
+fn buildCode(out_buf: []u8, secret: []const u8, counter: u64, digits: u8, algorithm: Algorithm) ![]u8 {
     if (digits < 6 or digits > 8) {
         return CodeGenError.OutOfBounds;
     }
@@ -78,24 +126,28 @@ fn buildCode(secret: []const u8, counter: u64, digits: u8, algorithm: Algorithm)
 
     switch (algorithm) {
         .Sha1 => {
-            const hmac = crypto.HmacSha1;
+            const hmac = crypto.auth.hmac.HmacSha1;
             var buffer: [hmac.mac_length]u8 = undefined;
+            var counter_buf: [8]u8 = undefined;
+            std.mem.writeIntBig(u64, &counter_buf, counter);
             var ctx = hmac.init(secret);
-            ctx.update(intToSlice(counter));
+            ctx.update(counter_buf[0..]);
             ctx.final(buffer[0..]);
             buf = buffer[0..buffer.len];
         },
         .Sha256 => {
-            const hmac = crypto.HmacSha256;
+            const hmac = crypto.auth.hmac.sha2.HmacSha256;
             var buffer: [hmac.mac_length]u8 = undefined;
+            var counter_buf: [8]u8 = undefined;
+            std.mem.writeIntBig(u64, &counter_buf, counter);
             var ctx = hmac.init(secret);
-            ctx.update(intToSlice(counter));
+            ctx.update(counter_buf[0..]);
             ctx.final(buffer[0..]);
             buf = buffer[0..buffer.len];
         },
-        else => {
-            return CodeGenError.UnsupportedAlgorithm;
-        },
+        // else => {
+        //     return CodeGenError.UnsupportedAlgorithm;
+        // },
     }
 
     // Truncate HS (HS = Hmac(key, counter))
@@ -108,59 +160,96 @@ fn buildCode(secret: []const u8, counter: u64, digits: u8, algorithm: Algorithm)
 
     // add padding to the left incase the first number is a 0
     const code = bin_code % std.math.pow(u32, 10, digits);
-    return formatCode(code, digits);
-}
-
-/// Creates a byte slice from the given integer
-fn intToSlice(val: u64) []const u8 {
-    var bytes: [8]u8 = undefined;
-    std.mem.writeIntBig(u64, &bytes, val);
-    return bytes[0..];
+    const len = formatCode(out_buf, code, digits);
+    // std.mem.copy(u8, out_buf, result[0..digits]);
+    return out_buf[0..len];
 }
 
 /// formatCode will try to parse the integer and return a string.
 /// An extra `0` will be added to the left to match the given length.
-fn formatCode(val: u64, length: u8) []u8 {
-    var buf: [8]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buf);
-    std.fmt.formatIntValue(val, "", std.fmt.FormatOptions{ .width = length, .fill = '0' }, fbs.outStream()) catch unreachable;
-    return fbs.getWritten();
+fn formatCode(buf: []u8, val: u32, length: u8) usize {
+    const len = std.fmt.formatIntBuf(buf, val, 10, .lower, std.fmt.FormatOptions{ .width = length, .fill = '0' });
+    return len;
 }
 
 test "HOTP code generation" {
-    const hotp = Hotp.init();
+    var buf: [8]u8 = undefined;
+    const hotp = Hotp.init(&buf);
     const code = try hotp.generateCode("secretkey", 0);
-    std.testing.expectEqualSlices(u8, "049381", code);
+    try std.testing.expectEqualSlices(u8, "049381", code);
 }
 
 test "HOTP 8 digits" {
-    var hotp = Hotp.init();
+    var buf: [8]u8 = undefined;
+    var hotp = Hotp.init(&buf);
     hotp.digits = 8;
     const code = try hotp.generateCode("secretkey", 0);
-    std.testing.expectEqualSlices(u8, "74049381", code);
+    try std.testing.expectEqualSlices(u8, "74049381", code);
 }
 
 test "HOTP different counters" {
-    const hotp = Hotp.init();
+    var buf: [8]u8 = undefined;
+    const hotp = Hotp.init(&buf);
     var code = try hotp.generateCode("secretkey", 1);
-    std.testing.expectEqualSlices(u8, "534807", code);
+    try std.testing.expectEqualSlices(u8, "534807", code);
 
     code = try hotp.generateCode("secretkey", 2);
-    std.testing.expectEqualSlices(u8, "155320", code);
+    try std.testing.expectEqualSlices(u8, "155320", code);
 
     code = try hotp.generateCode("secretkey", 3);
-    std.testing.expectEqualSlices(u8, "642297", code);
+    try std.testing.expectEqualSlices(u8, "642297", code);
 
     code = try hotp.generateCode("secretkey", 4);
-    std.testing.expectEqualSlices(u8, "964223", code);
+    try std.testing.expectEqualSlices(u8, "964223", code);
 
     code = try hotp.generateCode("secretkey", 5);
-    std.testing.expectEqualSlices(u8, "416848", code);
+    try std.testing.expectEqualSlices(u8, "416848", code);
 }
 
 test "TOTP code generation" {
-    const totp = Totp.init(Options{});
+    var buf: [8]u8 = undefined;
+    var totp = Totp.init(&buf, Options{
+        .algorithm = .Sha1,
+        .digits = 8,
+    });
     const time = 1587915766;
-    const code = try totp.generateCode("secretkey", time);
-    std.testing.expectEqualSlices(u8, "623043", code);
+    var code = try totp.generateCode("secretkey", time);
+    try std.testing.expectEqualSlices(u8, "68623043", code);
+    // Test data from https://www.rfc-editor.org/rfc/rfc6238 Appendix B.
+    code = try totp.generateCode("12345678901234567890", 59);
+    try std.testing.expectEqualSlices(u8, "94287082", code);
+    code = try totp.generateCode("12345678901234567890", 1111111109);
+    try std.testing.expectEqualSlices(u8, "07081804", code);
+    code = try totp.generateCode("12345678901234567890", 1111111111);
+    try std.testing.expectEqualSlices(u8, "14050471", code);
+    code = try totp.generateCode("12345678901234567890", 1234567890);
+    try std.testing.expectEqualSlices(u8, "89005924", code);
+    code = try totp.generateCode("12345678901234567890", 2000000000);
+    try std.testing.expectEqualSlices(u8, "69279037", code);
+    code = try totp.generateCode("12345678901234567890", 20000000000);
+    try std.testing.expectEqualSlices(u8, "65353130", code);
+
+    const totp_256 = Totp.init(&buf, Options{
+        .algorithm = .Sha256,
+        .digits = 8,
+    });
+    code = try totp_256.generateCode("12345678901234567890123456789012", 59);
+    try std.testing.expectEqualSlices(u8, "46119246", code);
+    code = try totp_256.generateCode("12345678901234567890123456789012", 1111111109);
+    try std.testing.expectEqualSlices(u8, "68084774", code);
+    code = try totp_256.generateCode("12345678901234567890123456789012", 1111111111);
+    try std.testing.expectEqualSlices(u8, "67062674", code);
+    code = try totp_256.generateCode("12345678901234567890123456789012", 1234567890);
+    try std.testing.expectEqualSlices(u8, "91819424", code);
+    code = try totp_256.generateCode("12345678901234567890123456789012", 2000000000);
+    try std.testing.expectEqualSlices(u8, "90698825", code);
+    code = try totp_256.generateCode("12345678901234567890123456789012", 20000000000);
+    try std.testing.expectEqualSlices(u8, "77737706", code);
+}
+
+test "Base32 decode" {
+    var buf: [40]u8 = undefined;
+    const result = decodeBase32(&buf, "BAFYBEICZSSCDSBS7FFQZ55ASQDF3SMV6KLCW3GOFSZVWLYARCI47BGF354");
+    const key = [_]u8{ 0x08, 0x0b, 0x80, 0x91, 0x02, 0xcc, 0xa4, 0x21, 0xc8, 0x32, 0xf9, 0x4b, 0x0c, 0xf7, 0xa0, 0x94, 0x06, 0x5d, 0xc9, 0x95, 0xf2, 0x96, 0x2b, 0x6c, 0xce, 0x2c, 0xb3, 0x5b, 0x2f, 0x00, 0x88, 0x91, 0xcf, 0x84, 0xc5, 0xdf };
+    try std.testing.expectEqualSlices(u8, &key, result);
 }
